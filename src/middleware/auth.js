222b54a5ea26cb9ca4d5f962d42f8ad260fc2542
Adi <adiswork066@gmail.com>
2026-04-22 22:02:40 +0530

    updated

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -18,7 +18,7 @@ module.exports = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, "secret");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
 
     console.log("Token verified");
